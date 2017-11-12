3936236e6e6bc173701cecdf03176f956a22113e
etochy <etochy@gmail.com>
2017-11-12 18:05:07 +0100

    Student A : Configure front end resources

---
+++ alma-server/src/main/resources/static/folder/greatFunction.js
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2017 siestsa.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License which
+ * accompanies this distribution, and is available at
+ * https://opensource.org/licenses/MIT
+ *******************************************************************************/
+(function () {
+    'use strict';
+
+    var greatFunction = function () {
+        alert('This is great!');
+    };
+    
+    module.exports = greatFunction;
+})();
---
+++ alma-server/src/main/resources/static/main.js
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2017 siestsa.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License which
+ * accompanies this distribution, and is available at
+ * https://opensource.org/licenses/MIT
+ *******************************************************************************/
+(function () {
+    'use strict';
+
+    var greatFunction = require('./folder/greatFunction');
+    greatFunction();
+})();
